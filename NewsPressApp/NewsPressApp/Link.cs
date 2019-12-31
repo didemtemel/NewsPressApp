68fce49f32c2d56615668cd2052565ea6227d18e
didemtemel <44496208+didemtemel@users.noreply.github.com>
2019-12-31 08:48:52 +0300

    Link.cs is created and connected to DB

---
+++ NewsPressApp/NewsPressApp/Link.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPressApp
+{
+    public class Link
+    {
+        public int id { get; set; }
+
+        public string SiteCode { get; set; }
+
+        public string SiteLink { get; set; }
+
+        public string NewsType { get; set; }
+
+        public DateTime  NewsDate { get; set; }
+
+
+        public string NewsTitle { get; set; }
+
+        public string NewsDescription { get; set; }
+
+        public string NewsContent { get; set; }
+    }
+}
